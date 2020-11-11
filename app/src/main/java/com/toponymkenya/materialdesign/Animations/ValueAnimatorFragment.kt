f9cd809b8c149d394abc63ea61bf348597c0bb5d
Wachira Nduati <wachiranduati@gmail.com>
2020-11-11 21:27:41 +0300

    implement value animator

--- app/src/main/java/com/toponymkenya/materialdesign/Animations/ValueAnimatorFragment.kt
+++ app/src/main/java/com/toponymkenya/materialdesign/Animations/ValueAnimatorFragment.kt
@@ -1,34 +1,18 @@
 package com.toponymkenya.materialdesign.Animations
 
+import android.animation.ValueAnimator
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.color.MaterialColors.getColor
 import com.toponymkenya.materialdesign.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ValueAnimatorFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ValueAnimatorFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,25 +20,31 @@ class ValueAnimatorFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_value_animator, container, false)
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ValueAnimatorFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ValueAnimatorFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val animatedText = view.findViewById<TextView>(R.id.valAnimTextView)
+        val animButton = view.findViewById<Button>(R.id.valAnimBtn)
+
+        val posY = ValueAnimator.ofFloat(0f, 200f).apply {
+            duration = 5000
+            addUpdateListener {
+                animatedText.translationY = it.animatedValue as Float
             }
+        }
+
+        val rgbC = ValueAnimator.ofArgb(Color.BLACK, Color.CYAN).apply {
+            duration = 5000
+            addUpdateListener {
+                animatedText.setTextColor(it.animatedValue as Int)
+            }
+        }
+
+        animButton.setOnClickListener {
+            posY.start()
+            rgbC.start()
+        }
     }
 }
