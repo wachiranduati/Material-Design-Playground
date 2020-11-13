a248d2d37ee340a52905e99196f37e631e399e16
Wachira Nduati <wachiranduati@gmail.com>
2020-11-13 21:08:16 +0300

    complete implementation of the material input types

--- app/src/main/java/com/toponymkenya/materialdesign/input/InputFragment.kt
+++ app/src/main/java/com/toponymkenya/materialdesign/input/InputFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.google.android.material.textfield.TextInputLayout
 import com.toponymkenya.materialdesign.R
 
 class InputFragment : Fragment() {
@@ -17,4 +20,11 @@ class InputFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_input, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layout = view.findViewById<TextInputLayout>(R.id.dropdown_input)
+        val items = listOf("Material", "Design", "Components", "Android")
+        val adapter = ArrayAdapter(requireContext(), R.layout.item_input_dropdown, items)
+        (layout.editText as? MaterialAutoCompleteTextView)?.setAdapter(adapter)
+    }
 }
